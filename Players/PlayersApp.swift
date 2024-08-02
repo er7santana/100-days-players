05398cbbe72332b0cb394e057533d3d281d33a5b
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-02 07:44:15 -0300

    Initial Commit

---
+++ Players/PlayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayersApp.swift
+//  Players
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
